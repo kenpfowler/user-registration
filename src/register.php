5554f5649567a62d575100330ac0de7dfcf2fd14
kenpfowler <kenpfowler@gmail.com>
2025-05-09 18:39:24 +0000

    fail if required fields are empty

--- src/register.php
+++ src/register.php
@@ -2,15 +2,17 @@
 require '../config/db.php';
 session_start();
 
-// TODO: Understand this function
 function sanitize($value) {
     return htmlspecialchars(trim($value), ENT_QUOTES, 'UTF-8');
 }
 
 // ensure registration uses correct method
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // TODO: what are we doing here and why (sanitizing and validating)
     // sanitize inputs
+    // NOTE: we may want to think more about the value of sanitizing inputs here...
+    // for example: if a user has a lastname O'Brian, the ' would be stored in the database as an HTML entity.
+    // maybe that's fine because, upon reterieval, it would be displayed correctly.
+    // but maybe it's not ideal if we want to search the database for the users name and it is not stored as we expect
     $account_type = sanitize($_POST['account_type']);
     $username     = sanitize($_POST['username']);
     // no sanitize: raw input for hashing
@@ -18,13 +20,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $full_name    = sanitize($_POST['full_name']);
     $contact_title = isset($_POST['contact_title']) ? sanitize($_POST['contact_title']) : null;
     $phone_number = sanitize($_POST['phone_number']);
-    $email        = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $email        = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
     // TODO: once we've vaildated and sanitized the inputs we may want to do somthing with the values before we try to insert them into the database
 
-    if (!$email) {
-        // apparently die fucntion can be abused. Can we sent the user an alert if the email is incorrect?
-        die("Invalid email format.");
+
+    // fail if required data is not provided
+    if (empty($account_type) || empty($username) || empty($password) || empty($full_name) || empty($phone_number) || empty($email)) {
+    $_SESSION['error'] = "Please fill out all required fields.";
+    header('Location: /');
+    exit;
     }
+
 
     // https://www.php.net/manual/en/function.password-hash.php
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
