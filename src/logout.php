47fff651a3cec449ffc004ab097eef59ca9194d3
kenpfowler <kenpfowler@gmail.com>
2025-05-09 18:03:09 +0000

    feat: document and add cookie invalidation to logout

--- src/logout.php
+++ src/logout.php
@@ -1,7 +1,21 @@
 <?php
-// TODO: better understand what the session library is doing
 session_start();
+
+// Clear all session variables
 $_SESSION = [];
+
+// Invalidate the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destroy the session
 session_destroy();
+
+// Redirect user to homepage or login
 header('Location: /');
 exit;
