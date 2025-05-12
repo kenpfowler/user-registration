9473768f9b20ab8c04c80c686edc1d28e2bd6ce7
kenpfowler <kenpfowler@gmail.com>
2025-05-12 16:33:59 +0000

    feat: define controllers

---
+++ src/controllers/dashboard.php
@@ -0,0 +1,6 @@
+<?php
+$title = "Dashboard";
+require __DIR__ . '../views/partials/head.php';
+require __DIR__ . '../views/partials/header.php';
+require __DIR__ . '../views/dashboard.php';
+require __DIR__ . '../views/partials/footer.php';
